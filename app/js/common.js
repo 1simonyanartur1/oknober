f7364e4fe8cf60ba5b99b311b819d70a1a72a50e
Artur <1simonyanartur1@gmail.com>
2021-01-01 23:23:22 +0300

    add adaptive header

--- app/js/common.js
+++ app/js/common.js
@@ -1,17 +1,56 @@
 (function ($) {
 	$(document).ready(function () {
 
-		$('.fixed-links').on('mouseenter', function() {
+		// SmartMenus init
+		$(function () {
+			$('#main-menu').smartmenus({
+				mainMenuSubOffsetX: -1,
+				mainMenuSubOffsetY: 4,
+				subMenusSubOffsetX: 6,
+				subMenusSubOffsetY: -6,
+				markCurrentItem: true,
+				markCurrentTree: true,
+				collapsibleBehavior: 'link'
+			});
+		});
+
+		// SmartMenus mobile menu toggle button
+		$(function () {
+			var $mainMenuState = $('#main-menu-state');
+			if ($mainMenuState.length) {
+				// animate mobile menu
+				$mainMenuState.change(function (e) {
+					var $menu = $('#main-menu');
+					if (this.checked) {
+						$menu.hide().slideDown(250, function () {
+							$menu.css('display', '');
+						});
+					} else {
+						$menu.show().slideUp(250, function () {
+							$menu.css('display', '');
+						});
+					}
+				});
+				// hide mobile menu beforeunload
+				$(window).bind('beforeunload unload', function () {
+					if ($mainMenuState[0].checked) {
+						$mainMenuState[0].click();
+					}
+				});
+			}
+		});
+
+		$('.fixed-links').on('mouseenter', function () {
 			$(this).removeClass('enter');
 		});
-		$('.fixed-links').on('mouseleave', function() {
+		$('.fixed-links').on('mouseleave', function () {
 			$(this).addClass('enter');
 		});
 
-		$('.consult-icon').on('click', function() {
+		$('.consult-icon').on('click', function () {
 			$('.consult').removeClass('mini');
 		});
-		$('.consult__close').on('click', function() {
+		$('.consult__close').on('click', function () {
 			$('.consult').addClass('mini');
 		});
 
@@ -27,7 +66,7 @@
 			slidesToScroll: 1,
 			asNavFor: '.quiz .bottom-right'
 		});
-		
+
 		$('.quiz .bottom-right').slick({
 			slidesToShow: 1,
 			slidesToScroll: 1,
@@ -63,29 +102,29 @@
 		});
 
 
-		var handle = $( ".window-counter__handler" );
-    $( "#window-counter" ).slider({
+		var handle = $(".window-counter__handler");
+		$("#window-counter").slider({
 			value: 5,
-      orientation: "horizontal",
-      range: "min",
+			orientation: "horizontal",
+			range: "min",
 			animate: true,
 			max: 25,
-      create: function() {
-        handle.text( $( this ).slider( "value" ) );
-      },
-      slide: function( event, ui ) {
-        handle.text( ui.value );
-      }
-    });
+			create: function () {
+				handle.text($(this).slider("value"));
+			},
+			slide: function (event, ui) {
+				handle.text(ui.value);
+			}
+		});
 
 
 		var $minInput = $(".money__input[name='min-price']");
 		var $maxInput = $(".money__input[name='max-price']");
-		
+
 		var minPrice = 0;
 		var maxPrice = 200000;
 
-		var $rangeSlider = $( ".money-slider" );
+		var $rangeSlider = $(".money-slider");
 
 		$rangeSlider.slider({
 			range: true,
@@ -129,24 +168,22 @@
 			slidesToShow: 6,
 			slidesToScroll: 1,
 			infinite: true,
-			responsive: [
-				{
-					breakpoint: 1200,
-					settings: {
-						slidesToShow: 4,
-					}
-				}, {
-					breakpoint: 800,
-					settings: {
-						slidesToShow: 3,
-					}
-				}, {
-					breakpoint: 575,
-					settings: {
-						slidesToShow: 2,
-					}
+			responsive: [{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 4,
 				}
-			]
+			}, {
+				breakpoint: 800,
+				settings: {
+					slidesToShow: 3,
+				}
+			}, {
+				breakpoint: 575,
+				settings: {
+					slidesToShow: 2,
+				}
+			}]
 		});
 
 		$('.certificates .slider').slick({
@@ -175,20 +212,18 @@
 			nextArrow: '<button class="arrow arrow-next"><img src="../img/icons/arrow-bottom.svg"></button>',
 			focusOnSelect: true,
 			vertical: true,
-			responsive: [
-				{
-					breakpoint: 800,
-					settings: {
-						vertical: false
-					}
-				}, {
-					breakpoint: 575,
-					settings: {
-						vertical: false,
-						slidesToShow: 2
-					}
+			responsive: [{
+				breakpoint: 800,
+				settings: {
+					vertical: false
 				}
-			]
+			}, {
+				breakpoint: 575,
+				settings: {
+					vertical: false,
+					slidesToShow: 2
+				}
+			}]
 		});
 
 		$(".tabs").tabs();
